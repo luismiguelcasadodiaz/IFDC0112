b35fa48c7d6768f4d2f59ec7cc797a979423186f
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2025-03-14 00:47:31 +0100

    Split files for Power BI online session

---
+++ EDA/Spotify/data/25000_lines_each_file/extrae.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+head -n +2 spotify-streaming-top-50-argentina.csv > 25000_argentina.csv ; tail -25000 spotify-streaming-top-50-argentina.csv >> 25000_argentina.csv
+head -n +2 spotify-streaming-top-50-france.csv > 25000_france.csv ; tail -25000 spotify-streaming-top-50-france.csv >> 25000_france.csv
+head -n +2 spotify-streaming-top-50-italy.csv > 25000_italy.csv ; tail -25000 spotify-streaming-top-50-italy.csv >> 25000_italy.csv
+head -n +2 spotify-streaming-top-50-japan.csv > 25000_japan.csv ; tail -25000 spotify-streaming-top-50-japan.csv >> 25000_japan.csv
+head -n +2 spotify-streaming-top-50-mexico.csv > 25000_mexico.csv ; tail -25000 spotify-streaming-top-50-mexico.csv >> 25000_mexico.csv
+head -n +2 spotify-streaming-top-50-south-korea.csv > 25000_south-korea.csv ; tail -25000 spotify-streaming-top-50-south-korea.csv >> 25000_south-korea.csv
+head -n +2 spotify-streaming-top-50-spain.csv > 25000_spain.csv ; tail -25000 spotify-streaming-top-50-spain.csv >> 25000_spain.csv
+head -n +2 spotify-streaming-top-50-uk.csv > 25000_uk.csv ; tail -25000 spotify-streaming-top-50-uk.csv >> 25000_uk.csv
+head -n +2 spotify-streaming-top-50-usa.csv > 25000_usa.csv ; tail -25000 spotify-streaming-top-50-usa.csv >> 25000_usa.csv
+head -n +2 spotify-streaming-top-50-world.csv > 25000_world.csv ; tail -25000 spotify-streaming-top-50-world.csv >> 25000_world.csv
